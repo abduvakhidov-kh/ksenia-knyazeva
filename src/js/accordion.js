003cc98a4882ae4c4b44603c8b17b8dd1b34263f
Khabibullo Abduvakhidov <habibulloadbuvahidov03@gmail.com>
2024-05-10 20:04:36 +0500

    fixed: styles of favorite products list

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -5,19 +5,14 @@ for (let i = 0; i < acc.length; i++) {
     this.classList.toggle("active");
     const panel = this.parentElement.querySelector(".panel");
     const icon = this.querySelector(".icon");
-    console.log(panel, this, panel.scrollHeight, icon);
-    if (icon && panel.style.maxHeight) {
-      if (panel.style.maxHeight) {
-        icon.innerHTML = `<line x1="12" y1="1" x2="12" y2="23" style="stroke: #000; stroke-width: 1" />
-        <line x1="1" y1="12" x2="23" y2="12" style="stroke: #000; stroke-width: 1" />`;
-      } else {
-        icon.innerHTML = `<line x1="1" y1="12" x2="23" y2="12" style="stroke: #000; stroke-width: 1" />`;
-      }
-    }
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
+
+    if (!panel.style.maxHeight) {
+      icon.innerHTML = `<line x1="1" y1="12" x2="23" y2="12" style="stroke: #000; stroke-width: 1" />`;
       panel.style.maxHeight = panel.scrollHeight + "px";
+    } else {
+      icon.innerHTML = `<line x1="12" y1="1" x2="12" y2="23" style="stroke: #000; stroke-width: 1" />
+      <line x1="1" y1="12" x2="23" y2="12" style="stroke: #000; stroke-width: 1" />`;
+      panel.style.maxHeight = null;
     }
   });
 }
